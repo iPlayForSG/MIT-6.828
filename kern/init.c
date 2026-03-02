ce68af8ce0da4b84170ffb1193c48cf5aa77e055
iPlayForSG <iplayforsg@foxmail.com>
2026-03-02 01:40:15 -0800

    Reconstructed

--- kern/init.c
+++ kern/init.c
@@ -35,6 +35,18 @@ i386_init(void)
 
 	cprintf("6828 decimal is %o octal!\n", 6828);
 
+	// To Debug Lab 1 Exercise 8 Q3.
+	// cprintf("To Debug Lab 1 Exercise 8 Q3.\n");
+	// int x = 1, y = 3, z = 4;
+	// cprintf("x %d, y %x, z %d\n", x, y, z);
+
+	// To Print Lab 1 Exercise 8 Q4.
+	// unsigned int i = 0x00646c72;
+    // cprintf("H%x Wo%s\n", 57616, &i);
+
+	// To Debug Lab 1 Exercise 8 Q5.
+	cprintf("x=%d y=%d", 3);
+
 	// Test the stack backtrace function (lab 1 only)
 	test_backtrace(5);
 
