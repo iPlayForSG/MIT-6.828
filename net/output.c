6b51ec4cf71a6bda056d23b589a50c266ad485e0
iPlayForSG <iplayforsg@foxmail.com>
2026-04-26 02:22:14 -0700

    my solution for Lab 6 Exercise 8 & Question 1

--- net/output.c
+++ net/output.c
@@ -10,4 +10,21 @@ output(envid_t ns_envid)
 	// LAB 6: Your code here:
 	// 	- read a packet from the network server
 	//	- send the packet to the device driver
+	envid_t whom;
+	int perm;
+	int32_t req;
+
+	while (1) {
+		req = ipc_recv(&whom, &nsipcbuf, &perm);
+
+		// 只处理发自核心网络环境，且类型为输出请求的消息
+		if (req != NSREQ_OUTPUT || whom != ns_envid) {
+			continue;
+		}
+
+		struct jif_pkt *pkt = &(nsipcbuf.pkt);
+		while (sys_pkt_send(pkt->jp_data, pkt->jp_len) < 0) {
+			sys_yield();
+		}
+	}
 }
