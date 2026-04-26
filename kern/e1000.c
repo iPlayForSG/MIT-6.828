2ddac3c2cef3f4b8ddb6a3e15ad131aadee0e57d
iPlayForSG <iplayforsg@foxmail.com>
2026-04-26 00:47:36 -0700

    my solution for Lab 6 Exercise 4

--- kern/e1000.c
+++ kern/e1000.c
@@ -2,12 +2,17 @@
 #include <kern/pmap.h>
 
 // LAB 6: Your driver code here
+
+volatile uint32_t *e1000;
+
 int
 e1000_attach(struct pci_func *pcif)
 {
 	// 启用 PCI 设备，系统会自动配内存、IO 端口和 IRQ 中断号
 	pci_func_enable(pcif);
-	cprintf("E1000 attached.\n");
+    // Device Status Register
+    e1000 = (volatile uint32_t *) mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+	cprintf("E1000 status: 0x%08x\n", e1000[E1000_STATUS / sizeof(uint32_t)]);
 	
 	return 0;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -2,6 +2,12 @@
 #define JOS_KERN_E1000_H
 
 #include <kern/pci.h>
+
+extern volatile uint32_t *e1000;
+
+// Device Status Register, offset 8 bytes
+#define E1000_STATUS   0x00008
+
 int e1000_attach(struct pci_func *pcif);
 
 #endif  // SOL >= 6
